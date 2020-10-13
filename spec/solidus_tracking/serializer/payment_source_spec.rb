bdd3b0a7bb9c55545379070641d4bc087434ebdf
Nicolò Rebughini <nicolo.rebughini@gmail.com>
2020-10-13 17:34:45 +0200

    Fix recursive loop with persisted payment sources
    Trying to call `Hash#to_json` on the serialized output would cause an
    infinite recursive loop with the PaymentSource::Type set to a class.
    
    This could be easily missed in specs because using orders, even with
    payments, but not persisted to the database wouldn't raise any error.

--- spec/solidus_tracking/serializer/payment_source_spec.rb
+++ spec/solidus_tracking/serializer/payment_source_spec.rb
@@ -8,4 +8,12 @@ RSpec.describe SolidusTracking::Serializer::PaymentSource do
       expect(described_class.serialize(payment_source)).to be_instance_of(Hash)
     end
   end
+
+  describe 'the serialized output' do
+    it 'can be converted to JSON when the payment source is persisted to the database' do
+      credit_card = create(:credit_card)
+
+      expect { described_class.serialize(credit_card).to_json }.not_to raise_error
+    end
+  end
 end
