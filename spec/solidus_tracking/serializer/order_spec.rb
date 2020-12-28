109b5f9919195f9ba5c8966ae43f793715c264a5
Sean <seand7565@gmail.com>
2020-12-28 10:56:33 -0600

    Update order serializer to handle promotions correctly
    Previously, the order serializer would pull in all promotions regardless
    of whether they were eligible or not and display the promotion_code.
    This also causes breakage with order promotions that apply to all orders,
    as those promotions do not have promotion_codes.
    
    This only displays eligible promotions in the serializer, and if no
    promotion code exists, will set the promotion code to "all orders".

--- spec/solidus_tracking/serializer/order_spec.rb
+++ spec/solidus_tracking/serializer/order_spec.rb
@@ -7,5 +7,24 @@ RSpec.describe SolidusTracking::Serializer::Order do
 
       expect(described_class.serialize(order)).to be_instance_of(Hash)
     end
+
+    it 'uses default promotion code to promotions that apply to all orders' do
+      promotion = create(:promotion_with_order_adjustment, apply_automatically: true)
+      order = create(:order)
+
+      promotion.activate(order: order)
+
+      expect(described_class.serialize(order)).to include({"DiscountCode" => "all orders"})
+    end
+
+    it 'does not include ineligible discount codes' do
+      promotions = Array.new(2) { create(:promotion_with_order_adjustment, apply_automatically: true) }
+      order = create(:order)
+
+      promotions.each { |promo| promo.activate(order: order) }
+      order.updater.update
+
+      expect(described_class.serialize(order)["DiscountCode"]).not_to include("all orders, all orders")
+    end
   end
 end
